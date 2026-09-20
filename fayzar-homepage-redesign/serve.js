8a4ad4f979a13b8143ec9f562168bfdcb06b502b
fayzarcomputer <fayzar.computer@gmail.com>
2026-09-20 19:11:34 +0600

    Auto-update fayzar-homepage-redesign/serve.js via Fayzar Computer Sync

---
+++ fayzar-homepage-redesign/serve.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3005;
+const MIME_TYPES = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css; charset=utf-8',
+  '.js': 'application/javascript; charset=utf-8',
+  '.json': 'application/json; charset=utf-8',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.svg': 'image/svg+xml',
+  '.woff2': 'font/woff2',
+  '.woff': 'font/woff',
+  '.ttf': 'font/ttf'
+};
+
+const server = http.createServer((req, res) => {
+  let reqUrl = req.url.split('?')[0];
+  if (reqUrl === '/' || reqUrl === '') reqUrl = '/index.html';
+  
+  const filePath = path.join(__dirname, decodeURIComponent(reqUrl));
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = MIME_TYPES[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Fayzar Computer Homepage Redesign Server running at: http://localhost:${PORT}/`);
+});

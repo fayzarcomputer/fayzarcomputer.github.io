426ccb8eeedafb284d9aed5ec6a720ff363fbfb4
fayzarcomputer <fayzar.computer@gmail.com>
2026-09-20 19:17:38 +0600

    Auto-update scratch/test_dict_prototype.py via Fayzar Computer Sync

---
+++ scratch/test_dict_prototype.py
@@ -0,0 +1,11 @@
+import json
+import re
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+# Let's inspect some of the unique Bengali phrases extracted
+with open('scratch/full_untranslated_breakdown.py', 'r', encoding='utf-8') as f:
+    pass
+
+print("Prototyping dictionary structure...")
